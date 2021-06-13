ed2f37d24d649c9ab53026fd8ee833ba7533d90b
Brendan Cariota <brendan@cariota.com>
2021-06-13 02:51:45 -0500

    Reverted to right before google map integration becuase it was broken

---
+++ src/screens/SearchResultsMap/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SearchResultsMaps = () => {
+    return (
+        <View>
+            <Text>Map</Text>
+        </View>
+    )
+}
+
+export default SearchResultsMaps
